4332a168ea4797a61cc699944c0ff10903376c39
oshinikk <100458338+oshinikk@users.noreply.github.com>
2022-03-27 02:02:43 +0530

    aaa

--- js/allproducts.js
+++ js/allproducts.js
@@ -1,2 +1,16 @@
 
+function addToCart(idtag,price) {
+    var select = document.getElementById(idtag);
+    var value = select.options[select.selectedIndex].value;
+    console.log(value);
+    var totalPrice = price * value;
+    console.log(totalPrice);
+  
+    var checkoutPrice = document.getElementById('total-price').innerHTML;
+    var totalcheckoutPrice = parseFloat(checkoutPrice);
+    console.log(totalcheckoutPrice);
+  
+    totalcheckoutPrice = totalcheckoutPrice + totalPrice;
+    document.getElementById('total-price').innerHTML = totalcheckoutPrice;
+  }
   
