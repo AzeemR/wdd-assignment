2dcfa2c6f3b91c3dcfbd8d0d9acb761ab3bdd27d
Azeem Rashard <70148665+AzeemR@users.noreply.github.com>
2022-03-27 22:40:13 +0530

    Update allproducts.js

--- js/allproducts.js
+++ js/allproducts.js
@@ -1,3 +1,4 @@
+// add products to array
 
 let arr1 = []; // Product Title
 let arr2 = []; // Product Price
@@ -17,6 +18,7 @@ function prodDet(num) {
 
 }
 
+// calculate total
 function calc() {
     tot = 0;
     for (let i = 0; i < arr2.length; i++) {
@@ -29,6 +31,7 @@ function calc() {
     return tot;
 }
 
+// Validate
 function validation(){
     var fname = document.getElementById("fname").value;
     var lname = document.getElementById("lname").value;
@@ -69,6 +72,7 @@ function validation(){
     }
 }
 
+// Submit the cart
 function submit() {
     var tot = calc()
 
@@ -90,48 +94,6 @@ function submit() {
     document.getElementById("invoice").innerHTML = prevHTML + "<hr><div>Total : Rs. " + tot + "</div><hr>";
 
 }
-
-function validation(){
-
-    var fname = document.getElementById("fname").value;
-    var lname = document.getElementById("lname").value;
-    var email = document.getElementById("email").value;
-    var cntact = document.getElementById("cntact").value;
-    var address = document.getElementById("address").value;
-   
-    
-   
-    if(fname=="null" || fname=="" ){
-      alert("Please Provide Your First Name!");
-    }
-    
-    else if(lname=="null" || lname==""){
-      alert("Please Provide Your Last Name!");
-    }
-    else if(email=="null" || email==""){
-      alert("Please Provide the email address!"); 
-    }
-    else if(email.length < 8 || email.indexOf("@") == -1 || email.indexOf(".") == -1){
-        alert("You have entered an invalid email address");
-    }
-    else if(cntact=="null" || cntact==""){
-      alert("Please Provide a phone number!");
-    }
-    
-    else if(cntact.length != 10){
-         alert("You have entered an invalid phone number");
-    }
-    else if(address=="null" || address==""){
-        alert("Please Provide your address!");
-    }
-    else if(address.indexOf(",") == -1 || address.length < 7){
-        alert("You have entered an invalid address");
-    } 
-   
-    else{
-    alert("Dear " +fname+lname+ ", Your order has been requested successfully!! You will recieve an confirmation email to " +email+ " shortly.\n\n\n Thank You for shopping with us!!" );
-    }
-  }
 
 // Go Up
 // Show when scrolled down
